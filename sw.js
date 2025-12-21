d7a3d6e63a238e32951db4b2b1d70e027af0d222
corrymesionvn-boop <corrymesionvn@gmail.com>
2025-12-21 22:45:43 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gd-rauma-universal-v1';
+const CACHE_NAME = 'gd-rauma-v5-final';
 const ASSETS = [
   './',
   './index.html',
@@ -9,25 +9,10 @@ const ASSETS = [
   './music/music5.mp3'
 ];
 
-// Cài đặt: Tải hết tài nguyên vào cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Kích hoạt: Xóa cache cũ nếu có bản cập nhật
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((k) => k !== CACHE_NAME && caches.delete(k))
-    ))
-  );
-});
-
-// Phản hồi: Ưu tiên lấy từ Cache trước, không có mạng vẫn chạy được
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
