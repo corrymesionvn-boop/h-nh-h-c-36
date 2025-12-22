af361a758621b6d5877f1296fcdeeedf94eea5ca
corrymesionvn-boop <corrymesionvn@gmail.com>
2025-12-22 12:49:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'gd-rauma-369';
+const CACHE_NAME = 'hinh-hoc-36-v-update-99'; // Cứ mỗi lần update là phải đổi số này
 const ASSETS = [
   './',
   './index.html',
+  './rauma.png',
   './music/music1.mp3',
   './music/music2.mp3',
   './music/music3.mp3',
@@ -10,9 +11,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting(); // BUỘC SW MỚI CÓ HIỆU LỰC NGAY
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => {
+        if (k !== CACHE_NAME) return caches.delete(k); // XÓA SẠCH CACHE CŨ
+      })
+    ))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
